282ad64d70f21c3ff16013bcdc6e37a1d757ebb1
Jaskirat Billing <Billingjas22@gmail.com>
2019-03-11 14:30:02 -0700

    added more questions

--- trivia-game-java/public/main.js
+++ trivia-game-java/public/main.js
@@ -9,13 +9,13 @@ $( document ).ready(function() {
 		var q1 = document.getElementById("question1").value;
 		var q2 = document.getElementById("question2").value;
 		var q3 = document.getElementById("question3").value;
-		// var q4 = document.getElementById("question1").value;
-		// var q5 = document.getElementById("question1").value;
-		// var q6 = document.getElementById("question1").value;
-		// var q7 = document.getElementById("question1").value;
-		// var q8 = document.getElementById("question1").value;
-		// var q9 = document.getElementById("question1").value;
-		// var q10 = document.getElementById("question1").value;
+		var q4 = document.getElementById("question4").value;
+		var q5 = document.getElementById("question5").value;
+		var q6 = document.getElementById("question6").value;
+		var q7 = document.getElementById("question7").value;
+		var q8 = document.getElementById("question8").value;
+		var q9 = document.getElementById("question9").value;
+		var q10 = document.getElementById("question10").value;
 
 
 
@@ -32,18 +32,39 @@ $( document ).ready(function() {
 		if (q3 === "cold") {
 			correct++; 
 	}
+		if (q4 === "two") {
+			correct++; 
+	}
+		if (q5 === "none") {
+			correct++; 
+	}
+		if (q6 === "day breaks and night falls") {
+			correct++; 
+	}
+		if (q7 === "four") {
+			correct++; 
+	}
+		if (q8 === "the match") {
+			correct++; 
+	}
+		if (q9 === "stairs") {
+			correct++; 
+	}
+		if (q10 === "widow") {
+			correct++; 
+	}
 	
 
 
-	if (correct == 0) {
+	if (correct < 3) {
 		score = 0;
 	}
 
-	if (correct == 1 || correct == 2) {
+	if (correct > 3 || correct < 7) {
 		score = 1;
 	}
 
-	if (correct == 3) {
+	if (correct > 7) {
 		score = 2;
 	}
 }
